620df707b5438ef9185b2a95c97e6df5510ec287
Rocco <rocco.p246@gmail.com>
2021-01-07 00:41:14 +0100

    my first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,34 @@
-import * as mdb from 'mdb-ui-kit';
+// BOOTSTRAP CORE COMPONENTS
+import Button from '../../mdb/js/bootstrap/mdb-prefix/button';
+import Collapse from '../../mdb/js/bootstrap/mdb-prefix/collapse';
+import Alert from '../../mdb/js/free/alert';
+import Carousel from '../../mdb/js/free/carousel';
+import Modal from '../../mdb/js/free/modal';
+import Popover from '../../mdb/js/free/popover';
+import ScrollSpy from '../../mdb/js/free/scrollspy';
+import Tab from '../../mdb/js/free/tab';
+import Tooltip from '../../mdb/js/free/tooltip';
+import Toast from '../../mdb/js/free/toast';
 
-export default {
-  mdb,
+// MDB FREE COMPONENTS
+import Input from '../../mdb/js/free/input';
+import Dropdown from '../../mdb/js/free/dropdown';
+import Ripple from '../../mdb/js/free/ripple';
+import Range from '../../mdb/js/free/range';
+
+export {
+  Alert,
+  Button,
+  Carousel,
+  Collapse,
+  Dropdown,
+  Input,
+  Modal,
+  Popover,
+  Ripple,
+  ScrollSpy,
+  Tab,
+  Toast,
+  Tooltip,
+  Range,
 };
